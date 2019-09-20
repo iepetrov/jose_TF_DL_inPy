72020d3c0f62737e3e35abb5dad31e6d05b4d5c1
Ivailo <ipetrov@robarts.ca>
2019-09-20 06:47:22 -0400

    Ch. 27

--- TF_basics_3.py
+++ TF_basics_3.py
@@ -58,3 +58,31 @@ result.eval()
 
 # or run
 sess.run(result)
+
+
+n1 = tf.constant(1)
+n2 = tf.constant(2)
+
+n3 = n1 + n2
+
+with tf.Session() as sess:
+    result = sess.run(n3)
+print(result)
+
+
+# When you start TF, a default Graph is created, you can create additional graphs easily:
+print(tf.get_default_graph())
+
+g = tf.Graph()
+print(g)
+
+graph_one = tf.get_default_graph()
+graph_two = tf.Graph()
+
+graph_one is tf.get_default_graph()
+graph_two is tf.get_default_graph()
+
+with graph_two.as_default():
+    print(graph_two is tf.get_default_graph())
+
+graph_two is tf.get_default_graph()
