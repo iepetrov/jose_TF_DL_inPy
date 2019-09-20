db34c091270c4c75d27cf8c16836f320882b9a35
Ivailo <ipetrov@robarts.ca>
2019-09-19 20:09:55 -0400

    final

---
+++ TF_basics_1.py
@@ -0,0 +1,6 @@
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), '..\\..\FULL-TENSORFLOW-NOTES-AND-DATA (1)\Tensorflow-Bootcamp-master\02-TensorFlow-Basics'))
+	print(os.getcwd())
+except:
+	pass
