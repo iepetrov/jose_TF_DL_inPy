14d2c9eefe2e6e0ddf587f6fa2628a42b65a96ab
Ivailo <ipetrov@robarts.ca>
2019-09-19 20:22:07 -0400

    yyy

--- TF_basics_1.py
+++ TF_basics_1.py
@@ -1,6 +1,12 @@
-import os
-try:
-	os.chdir(os.path.join(os.getcwd(), '..\\..\FULL-TENSORFLOW-NOTES-AND-DATA (1)\Tensorflow-Bootcamp-master\02-TensorFlow-Basics'))
-	print(os.getcwd())
-except:
-	pass
+# import os
+# try:
+# 	os.chdir(os.path.join(os.getcwd(), '..\\..\FULL-TENSORFLOW-NOTES-AND-DATA (1)\Tensorflow-Bootcamp-master\02-TensorFlow-Basics'))
+# 	print(os.getcwd())
+# except:
+# 	pass
+
+
+import tensorflow as try:
+    pass
+finally:
+    pass
